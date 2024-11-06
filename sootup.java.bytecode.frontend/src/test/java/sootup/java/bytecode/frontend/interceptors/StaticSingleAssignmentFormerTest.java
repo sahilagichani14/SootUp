fd6ed82102bfd303ce0a30273cd5e9a43cde87ba
zunwang <wangzun666@gmail.com>
2024-11-06 11:37:04 +0100

    fix specified ssa case

--- sootup.java.bytecode.frontend/src/test/java/sootup/java/bytecode/frontend/interceptors/StaticSingleAssignmentFormerTest.java
+++ sootup.java.bytecode.frontend/src/test/java/sootup/java/bytecode/frontend/interceptors/StaticSingleAssignmentFormerTest.java
@@ -135,7 +135,6 @@ public class StaticSingleAssignmentFormerTest {
     assertEquals(expectedBodyString, builder.build().toString());
   }
 
-  @Disabled("zw: fix me!")
   @Test
   // todo: delete after correction, some blocks has redundant predecessors
   // todo: [$stack14 := @caughtexception ... throw $l14]
@@ -161,7 +160,6 @@ public class StaticSingleAssignmentFormerTest {
     // System.out.println(bodyAfterSSA);
   }
 
-  @Disabled("zw: Fix Me!")
   @Test
   public void testSSA3() {
     ClassType clazzType = factory.getClassType("ForLoopSSA");
