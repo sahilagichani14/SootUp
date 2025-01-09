82a2b61b8f76f0439f831a8f7c8f79bcbe5a76d7
sahilagichani <sahilagichani@gmail.com>
2025-01-09 12:46:30 +0100

    review changes

--- sootup.java.bytecode.frontend/src/test/java/sootup/java/bytecode/frontend/inputlocation/FixJars.java
+++ sootup.java.bytecode.frontend/src/test/java/sootup/java/bytecode/frontend/inputlocation/FixJars.java
@@ -17,6 +17,10 @@ import sootup.jimple.frontend.JimpleView;
 public class FixJars extends BaseFixJarsTest {
 
   @Test
+  /* Isolated testcase from:
+  * String jarDownloadUrl = "https://repo1.maven.org/maven2/io/github/adven27/exam-core/2024.0.10/exam-core-2024.0.10.jar";
+  * String methodSignature = "<io.github.adven27.concordion.extensions.exam.core.logger.LoggingFormatterExtension$LoggingFormatterListener: void afterExample(org.concordion.api.listener.ExampleEvent)>";
+  */
   public void executeexamcorejar() {
     AnalysisInputLocation inputLocation =
         new JimpleAnalysisInputLocation(
@@ -34,16 +38,5 @@ public class FixJars extends BaseFixJarsTest {
                       .get()
                       .getBody();
             });
-  }
-
-  @Test
-  public void executeaudiofileRCjar() {
-    String jarDownloadUrl =
-        "https://repo1.maven.org/maven2/de/sciss/audiofile_3.0.0-RC2/2.3.3/audiofile_3.0.0-RC2-2.3.3.jar";
-    String methodSignature =
-        "<de.sciss.audiofile.AudioFile$AsyncBasic: scala.concurrent.Future close()>";
-    JavaView javaView = supplyJavaView(jarDownloadUrl);
-    assertMethodConversion(javaView, methodSignature);
-    assertJar(javaView);
   }
 }
